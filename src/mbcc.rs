8aa75b6393aa419418200fd875122e6014f72553
pitust <piotr@stelmaszek.com>
2020-12-12 08:38:10 +0000

    fixed mbcc

--- src/mbcc.rs
+++ src/mbcc.rs
@@ -1,4 +1,10 @@
 
+macro_rules! cwd {
+    () => {
+        std::env::current_dir().unwrap().as_os_str().to_string_lossy()
+    };
+}
+
 fn main() {
     println!(" == ohbuild (mbcc mode) started == ");
     let a = std::env::args().nth(1).unwrap();
@@ -7,15 +13,15 @@ fn main() {
     let ldscript = include_str!("user.ld");
     let tgd = include_str!("target.json");
 
-    let cached_ld_script = "/tmp/ohbuild.cache/link.ld";
-    let cached_targetspec = "/tmp/ohbuild.cache/target.json";
-    let cargo_cache_dir = format!("{}/target/{}", std::env::current_dir().unwrap().as_os_str().to_string_lossy(), name);
+    let cached_ld_script = format!("{}/target/{}/link.ld", cwd!(), name);
+    let cached_targetspec = format!("{}/target/{}/target.json", cwd!(), name);
+    let cargo_cache_dir = format!("{}/target/{}", cwd!(), name);
     let out = format!("rootfs/bin/{}", name);
     std::fs::create_dir_all(cargo_cache_dir.clone()).unwrap();
-    std::fs::create_dir_all(format!("/tmp/ohbuild.cache/ns.dir/src")).unwrap();
-    std::fs::copy(a, "/tmp/ohbuild.cache/ns.dir/src/main.rs").unwrap();
+    std::fs::create_dir_all(format!("{}/target/{}/ns.dir/src", cwd!(), name)).unwrap();
+    std::fs::copy(a, format!("{}/target/{}/ns.dir/src/main.rs", cwd!(), name)).unwrap();
     std::fs::write(cached_ld_script.clone(), ldscript).unwrap();
-    std::fs::write("/tmp/ohbuild.cache/ns.dir/Cargo.toml", include_str!("cargo-mbcc.toml")).unwrap();
+    std::fs::write(format!("{}/target/{}/ns.dir/Cargo.toml", cwd!(), name), include_str!("cargo-mbcc.toml")).unwrap();
     std::fs::write(cached_targetspec.clone(), tgd).unwrap();
     println!("{}", cached_targetspec);
 
@@ -28,7 +34,7 @@ fn main() {
         .env("CARGO_TARGET_DIR", cargo_cache_dir.clone())
         .env("CARGO_BUILD_TARGET", cached_targetspec)
         .env("RUSTFLAGS",format!("-Clink-args=-T{} -Ccode-model=large", cached_ld_script))
-        .current_dir("/tmp/ohbuild.cache/ns.dir")
+        .current_dir(format!("{}/target/{}/ns.dir", cwd!(), name))
         .spawn()
         .unwrap()
         .wait()
